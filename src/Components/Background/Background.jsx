27f30c540ae656e5ff6b23050236e316d1913543
Abel Birhanu <abelabirhanu6@gmail.com>
2025-03-22 10:47:37 +0300

    third  commit

---
+++ src/Components/Background/Background.jsx
@@ -0,0 +1,28 @@
+import './Background.css'
+import video1 from '../../Assets/video1.mp4'
+import car1 from '../../Assets/car1.jpg'
+import dream_car from '../../Assets/dream_car.jpg'
+import car2 from '../../Assets/car2.jpeg'
+
+const Background = ({playStatus,heroCount}) => {
+    console.log("playStatus:", playStatus);
+
+    if (playStatus) {
+        return(
+        <video className='background fade-in' autoPlay loop unmuted>
+            <source src={video1} type='video/mp4' />
+        </video>
+        )
+    }
+    else if(heroCount===0){
+        return <img src={car1} alt="" className='background fade-in' />
+    }
+    else if(heroCount===1){
+        return <img src={dream_car} alt="" className='background fade-in' />
+    }
+    else if(heroCount===2){
+        return <img src={car2} alt="" className='background fade-in' />
+    }
+}
+
+export default Background
